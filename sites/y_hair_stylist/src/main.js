feee21b5e94f72be7b09a57295f2b36f4763b73e
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-19 13:18:48 -0300

    Update main.js

--- sites/y_hair_stylist/src/main.js
+++ sites/y_hair_stylist/src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import Element from 'element-ui';
 import App from './App.vue';
+import router from './router';
 import './registerServiceWorker';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -9,5 +10,6 @@ Vue.use(Element);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
