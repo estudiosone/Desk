573befe42695197e2779df41d902a92aeca664cb
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-19 13:18:57 -0300

    Update App.vue

--- sites/y_hair_stylist/src/App.vue
+++ sites/y_hair_stylist/src/App.vue
@@ -1,5 +1,35 @@
 <template>
-  <div id="app" />
+  <el-container>
+    <el-header style="display: flex; justify-content: space-between;">
+      <router-link to="/#">
+        <img
+          style="height: 44px; margin: 8px;"
+          src="http://yhairstylist.com/wp-content/uploads/2019/03/cropped-Asset-1.png"
+        >
+      </router-link>
+      <el-menu
+        mode="horizontal"
+        :router="true"
+      >
+        <el-menu-item index="/#salones">
+          Salones
+        </el-menu-item>
+        <el-menu-item index="/#agendate">
+          Agendate
+        </el-menu-item>
+        <el-menu-item index="/#promociones">
+          Promociones
+        </el-menu-item>
+        <el-menu-item index="/#noticias">
+          Noticias
+        </el-menu-item>
+        <el-menu-item index="/store">
+          Store
+        </el-menu-item>
+      </el-menu>
+    </el-header>
+    <router-view />
+  </el-container>
 </template>
 
 <script>
@@ -8,11 +38,22 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import url('https://fonts.googleapis.com/css?family=Dosis');
 body {
   position: absolute;
   margin: 0;
   width: 100%;
   height: 100%;
+  font-family: 'Dosis', sans-serif;
+}
+
+.el-menu--horizontal.el-menu {
+  font-weight: 600;
+  text-transform: uppercase;
+  border-bottom: none !important;
+}
+.el-menu--horizontal>.el-menu-item {
+  color: #000 !important;
+  border-bottom: none !important;
 }
 </style>
