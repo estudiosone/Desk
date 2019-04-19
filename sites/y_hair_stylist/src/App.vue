3caa237b55d9e5a5a5a6f1d723e7974ceba3c337
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-19 11:59:03 -0300

    Create App.vue

---
+++ sites/y_hair_stylist/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" />
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+
+body {
+  position: absolute;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
