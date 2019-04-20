45f28debd3031b5b75e6b83d3e7a215c92fc1d2a
Diego Rodriguez <drodriguez@eldescubrimiento.local>
2019-04-19 21:42:19 -0300

    Update Home.vue

--- sites/y_hair_stylist/src/views/Home.vue
+++ sites/y_hair_stylist/src/views/Home.vue
@@ -18,28 +18,29 @@
     </el-row>
     <el-row id="salones">
       <el-row
-        :gutter="16"
         id="salon-brunel"
       >
         <h1>Yhair Stylist | Brunel</h1>
-        <el-col :span="16">
+        <el-col :span="8">
           <iframe
             src="https://snazzymaps.com/embed/149768"
             style="border:none;"
           />
         </el-col>
-        <el-col :span="8">
-          <el-row>
-            <el-col :span="8">
-              <span>
-                Dirección:
-              </span>
-            </el-col>
-            <el-col :span="16">
-              <span>
-                Cno. Carrasco 6955 local 101
-              </span>
-            </el-col>
+        <el-col :span="8" style="margin: 0 16px;">
+          <el-row type="flex" align="middle" style="margin-bottom: 32px;">
+            <img src="https://img.icons8.com/material-outlined/48/000000/marker.png">
+            <div style="margin: 11px 16px; font-size: 24px;">Cno. Carrasco 6955 local 101</div>
+          </el-row>
+          <el-row type="flex" align="middle" style="margin-bottom: 32px;">
+            <img src="https://img.icons8.com/material-outlined/48/000000/phone.png">
+            <div style="margin: 11px 16px; font-size: 24px;">+598 2605 9344</div>
+          </el-row>
+          <el-row type="flex" align="middle" style="margin-bottom: 32px;">
+            <img src="https://img.icons8.com/material-outlined/48/000000/clock.png">
+            <div style="margin: 11px 16px; font-size: 24px;">
+            <div>L: 14:00 a 19:00</div>
+            <div>M a S: 10:00 a 19:00</div></div>
           </el-row>
         </el-col>
       </el-row>
