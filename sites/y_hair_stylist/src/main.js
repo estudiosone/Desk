e50d35290aedbdd2ec38ad7319958379e7d6d8cd
Diego Rodriguez <drodriguez@eldescubrimiento.local>
2019-04-19 21:01:04 -0300

    Update main.js

--- sites/y_hair_stylist/src/main.js
+++ sites/y_hair_stylist/src/main.js
@@ -1,10 +1,10 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import Vue from 'vue';
 import Element from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/display.css';
 
 Vue.use(Element);
 Vue.config.productionTip = false;
