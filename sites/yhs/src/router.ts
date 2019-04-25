f87a344996383b410ab00bc4390d96596ccb6aaf
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-25 13:12:55 -0300

    Create router.ts

---
+++ sites/yhs/src/router.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+        // , offset: { x: 0, y: 10 }
+      };
+    }
+  },
+});
+
+export default router;
