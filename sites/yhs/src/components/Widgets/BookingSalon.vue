78dbe03ca26f70adf4a1316c5f152d5f0b1bfbb2
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-25 13:12:49 -0300

    Create BookingSalon.vue

---
+++ sites/yhs/src/components/Widgets/BookingSalon.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="widget-booking-salon">
+    <h2>{{ title }}</h2>
+    <el-form id="step-0">
+      <el-form-item label="Fecha" id="input-date">
+        <el-date-picker
+          v-model="bookingDate"
+          type="date"
+          align="center"
+          format="dd-MM-yyyy"
+          placeholder="Pick a day"
+          :picker-options="datePickerOptions"/>
+      </el-form-item>
+      <el-form-item id="btn-reservar">
+        <el-button @click="dialogBookingVisible = true">Reservar</el-button>
+      </el-form-item>
+    </el-form>
+    <el-dialog title="Agendate en nuestros salones" :visible.sync="dialogBookingVisible">
+      <el-collapse v-model="collapseValue" accordion>
+        <el-form 
+          label-width="60px"
+          label-position="left">
+          <el-collapse-item title="Tu reserva" name="1">
+            <el-form-item label="Fecha">
+              <el-date-picker
+                v-model="bookingDate"
+                type="date"
+                align="center"
+                format="dd-MM-yyyy"
+                placeholder="Pick a day"
+                :picker-options="datePickerOptions"/>
+            </el-form-item>
+            <el-form-item label="Hora">
+              <el-select v-model="bookingHour" placeholder="Seleccione una hora">
+                <el-option
+                  v-for="item in hourList"
+                  :key="item.value"
+                  :label="item.value"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="Salón">
+              <el-select v-model="bookingSalon" placeholder="Seleccione un salón">
+                <el-option
+                  v-for="item in salonList"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="Salón">
+              <el-select v-model="bookingService" placeholder="Seleccione un servicio">
+                <el-option
+                  v-for="item in serviceList"
+                  :key="item.value"
+                  :label="item.value"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button :disabled="bookingClientDataActive" @click="collapseValue = '2'">Siguiente</el-button>
+            </el-form-item>
+
+          </el-collapse-item>
+          <el-collapse-item :disabled="bookingClientDataActive" title="Tus datos" name="2">
+            <el-form-item label="Nombre">
+              <el-input v-model="customerName"></el-input>
+            </el-form-item>
+            <el-form-item label="Apellido">
+              <el-input v-model="customerSurname"></el-input>
+            </el-form-item>
+            <el-form-item label="Teléfono">
+              <el-input v-model="customerPhone"></el-input>
+            </el-form-item>
+            <el-form-item label="Email">
+              <el-input v-model="customerEmail"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button :disabled="bookingResumeDataActive" @click="collapseValue = '3'">Siguiente</el-button>
+            </el-form-item>
+          </el-collapse-item>
+          <el-collapse-item :disabled="bookingResumeDataActive" title="Confirmá tus datos" name="3">
+
+          </el-collapse-item>
+        </el-form>
+      </el-collapse>
+    </el-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      dialogBookingVisible: false,
+      collapseValue: '1',
+      bookingDate: new Date(),
+      bookingHour: undefined,
+      bookingSalon: undefined,
+      bookingService: undefined,
+      customerName: undefined,
+      customerSurname: undefined,
+      customerPhone: undefined,
+      customerEmail: undefined,
+      hourList: [
+        { value: '08:00' },
+        { value: '08:30' },
+        { value: '09:00' },
+        { value: '09:30' },
+        { value: '10:00' },
+        { value: '10:30' },
+        { value: '11:00' },
+        { value: '11:30' },
+        { value: '12:00' },
+        { value: '12:30' },
+        { value: '13:00' },
+        { value: '13:30' },
+        { value: '14:00' },
+        { value: '14:30' },
+        { value: '15:00' },
+        { value: '15:30' },
+        { value: '16:00' },
+        { value: '16:30' },
+        { value: '17:00' },
+        { value: '17:30' },
+        { value: '18:00' },
+        { value: '18:30' },
+        { value: '19:00' },
+        { value: '19:30' },
+        { value: '20:00' },
+        { value: '20:30' },
+      ],
+      salonList: [
+        {
+          value: 'portones',
+          label: 'Portones Shopping'
+        },
+        {
+          value: 'brunel',
+          label: 'Portal Brunel'
+        },
+        {
+          value: 'americas',
+          label: 'Portal Americas'
+        }
+      ],
+      serviceList: [
+        {
+          value: 'Lavado L’Oréal'
+        },
+        {
+          value: 'Lavado Kerastase'
+        },
+        {
+          value: 'Lavado Redken'
+        },
+        {
+          value: 'Brushing'
+        },
+      ],
+      datePickerOptions: {
+        disabledDate (date: any) {
+          const d = new Date();
+          d.setDate(d.getDate() - 1)
+          return date <= d
+        }
+      }
+    }
+  },
+  computed: {
+    bookingClientDataActive() {
+      if (this.bookingDate && this.bookingHour && this.bookingSalon && this.bookingService) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    bookingResumeDataActive() {
+      if (this.customerName && this.customerSurname && this.customerPhone && this.customerEmail) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Agendate'
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+#step-0 {
+  display: grid;
+  grid-template-columns: 300px 150px;
+  grid-template-rows: 1fr;
+  width: 450px;
+  margin: 0 auto;
+}
+</style>
