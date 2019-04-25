9384664210e3716ecdb79275697c21f894233a97
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-25 09:14:47 -0300

    Update SalonInfo.vue

--- sites/y_hair_stylist/src/components/SalonInfo.vue
+++ sites/y_hair_stylist/src/components/SalonInfo.vue
@@ -1,58 +1,58 @@
 <template>
   <el-row class="info">
-        <el-row
-          type="flex"
-          align="middle"
-          style="margin-bottom: 16px;"
+    <el-row
+      type="flex"
+      align="middle"
+      style="margin-bottom: 16px;"
+    >
+      <img src="https://img.icons8.com/material-outlined/24/000000/marker.png">
+      <div style="margin: 3px 16px; font-size: 18px;">
+        {{ address }}
+      </div>
+    </el-row>
+    <el-row
+      type="flex"
+      align="middle"
+      style="margin-bottom: 16px;"
+    >
+      <img src="https://img.icons8.com/material-outlined/24/000000/phone.png">
+      <div style="margin: 3px 16px; font-size: 18px;">
+        {{ phone }}
+      </div>
+    </el-row>
+    <el-row
+      type="flex"
+      align="middle"
+      style="margin-bottom: 16px;"
+    >
+      <img src="https://img.icons8.com/material-outlined/24/000000/clock.png">
+      <div style="margin: 3px 16px; font-size: 18px;">
+        <div
+          v-for="schedule in data.schedules"
+          :key="schedules.indexOf(schedule)"
         >
-          <img src="https://img.icons8.com/material-outlined/24/000000/marker.png">
-          <div style="margin: 3px 16px; font-size: 18px;">
-            {{ address }}
-          </div>
-        </el-row>
-        <el-row
-          type="flex"
-          align="middle"
-          style="margin-bottom: 16px;"
-        >
-          <img src="https://img.icons8.com/material-outlined/24/000000/phone.png">
-          <div style="margin: 3px 16px; font-size: 18px;">
-            {{ phone }}
-          </div>
-        </el-row>
-        <el-row
-          type="flex"
-          align="middle"
-          style="margin-bottom: 16px;"
-        >
-          <img src="https://img.icons8.com/material-outlined/24/000000/clock.png">
-          <div style="margin: 3px 16px; font-size: 18px;">
-            <div
-              v-for="schedule in data.schedules"
-              :key="schedules.indexOf(schedule)"
-            >
-              {{ schedule }}
-            </div>
-          </div>
-        </el-row>
+          {{ schedule }}
+        </div>
+      </div>
+    </el-row>
 
-        <el-row>
-          <el-button-group style="width: 100%">
-            <el-button
-              style="width: 50%"
-              type="primary"
-            >
-              Llamar
-            </el-button>
-            <el-button
-              style="width: 50%"
-              type="primary"
-            >
-              Ir
-            </el-button>
-          </el-button-group>
-        </el-row>
-      </el-row>
+    <el-row>
+      <el-button-group style="width: 100%">
+        <el-button
+          style="width: 50%"
+          type="primary"
+        >
+          Llamar
+        </el-button>
+        <el-button
+          style="width: 50%"
+          type="primary"
+        >
+          Ir
+        </el-button>
+      </el-button-group>
+    </el-row>
+  </el-row>
 </template>
 
 <script>
@@ -64,4 +64,3 @@ export default {
   },
 };
 </script>
-
