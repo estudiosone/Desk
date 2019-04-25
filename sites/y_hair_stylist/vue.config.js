c78e29ac84f941b15d8524775d65dff66c167599
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-04-25 13:11:19 -0300

    Update vue.config.js

--- sites/y_hair_stylist/vue.config.js
+++ sites/y_hair_stylist/vue.config.js
@@ -3,20 +3,4 @@ module.exports = {
     name: 'Yhair Stylist',
     themeColor: '#A52518',
   },
-
-  configureWebpack: {
-    performance: {
-      hints: 'warning', // enum
-      maxAssetSize: 1048576, // int (in bytes),
-      maxEntrypointSize: 1048576, // int (in bytes)
-    },
-  },
-
-  publicPath: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: false,
-  parallel: undefined,
-  css: undefined,
 };
