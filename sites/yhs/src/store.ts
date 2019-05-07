6b50bbd27c4e2decb3f4e8d5597e2e04aa69fd41
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-07 16:55:32 -0300

    Update store.ts

--- sites/yhs/src/store.ts
+++ sites/yhs/src/store.ts
@@ -5,10 +5,35 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    userId: '',
+    user: {
+      name: '',
+      surname: '',
+      email: '',
+      photoUrl: '',
+      phone: {
+        area_code: '',
+        number: '',
+      },
+      identification: {
+        type: '',
+        number: '',
+      },
+      address: {
+        street_name: '',
+        street_number: '',
+        zip_code: '',
+      },
+      isAnonymous: false,
+    }
   },
   mutations: {
-
+    userId(state, UID) {
+      state.userId = UID;
+    },
+    user(state, userData) {
+      state.user = userData;
+    }
   },
   actions: {
 
