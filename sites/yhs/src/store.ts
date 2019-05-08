64773992db8229783640408d7d898e7b45114b60
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-08 20:50:38 -0300

    Update store.ts

--- sites/yhs/src/store.ts
+++ sites/yhs/src/store.ts
@@ -3,10 +3,46 @@ import Vuex from 'vuex';
 import firebase from 'firebase';
 
 import { IState } from '../types/store';
+type Phone = {
+  area_code: any,
+  number: any,
+}
+type Identification = {
+  type: any,
+  number: any,
+}
+type Address = {
+  street_name: any,
+  street_number: any,
+  zip_code: any,
+}
+type User = {
+  name: any,
+  surname: any,
+  email: any,
+  photoUrl: any,
+  phone: Phone,
+  identification: Identification,
+  address: Address,
+}
+type OrderDetail = {
+  item: any,
+  quantity: any,
+  price: any,
+}
+type Order = {
+  detail: OrderDetail[],
+}
+type State = {
+  businessId: any,
+  userId: any,
+  user: User,
+  order: Order,
+}
 
 Vue.use(Vuex);
 
-const state = {
+const state: State = {
   businessId: 'hN4Z7KaHwWxniNgVHjTX',
   userId: '',
   user: {
@@ -44,13 +80,13 @@ export default new Vuex.Store({
     user(state, userData) {
       state.user = userData;
     },
+    order(state, payload: Order) {
+      state.order = payload;
+    },
+    order_add_detail_line(state, payload: OrderDetail) {
+      state.order.detail.push(payload);
+    }
   },
   actions: {
-    // async addToBag(context, payload) {
-    //   context.state.order.detail.push({
-    //     productId: payload.productId,
-    //     quantity: payload.quantity,
-    //   })
-    // },
   },
 });
