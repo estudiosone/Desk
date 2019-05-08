eaad94d91f55403e9dfdf8fd156e0e11f3bea254
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-07 21:51:38 -0300

    Update store.ts

--- sites/yhs/src/store.ts
+++ sites/yhs/src/store.ts
@@ -2,43 +2,55 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import firebase from 'firebase';
 
+import { IState } from '../types/store';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    userId: '',
-    user: {
-      name: '',
-      surname: '',
-      email: '',
-      photoUrl: '',
-      phone: {
-        area_code: '',
-        number: '',
-      },
-      identification: {
-        type: '',
-        number: '',
-      },
-      address: {
-        street_name: '',
-        street_number: '',
-        zip_code: '',
-      },
-      isAnonymous: false,
-    }
+const state = {
+  businessId: 'hN4Z7KaHwWxniNgVHjTX',
+  userId: '',
+  user: {
+    name: '',
+    surname: '',
+    email: '',
+    photoUrl: '',
+    phone: {
+      area_code: '',
+      number: '',
+    },
+    identification: {
+      type: '',
+      number: '',
+    },
+    address: {
+      street_name: '',
+      street_number: '',
+      zip_code: '',
+    },
   },
+  order: {
+    detail: [
+
+    ],
+  },
+};
+
+export default new Vuex.Store({
+  state,
   mutations: {
     userId(state, UID) {
       state.userId = UID;
     },
     user(state, userData) {
       state.user = userData;
-    }
+    },
   },
   actions: {
-    addToBag(context, id) {
-
-    },
+    // async addToBag(context, payload) {
+    //   context.state.order.detail.push({
+    //     productId: payload.productId,
+    //     quantity: payload.quantity,
+    //   })
+    // },
   },
 });
