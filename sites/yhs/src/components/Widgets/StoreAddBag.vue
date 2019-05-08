5ae4c064d48cc0ee69ed4e1eed1a658b95dd4cb5
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-08 12:48:34 -0300

    Create StoreAddBag.vue

---
+++ sites/yhs/src/components/Widgets/StoreAddBag.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="widget-store-add-bag">
+    <div class="section-button">
+      <div
+        class="price"
+        v-if="unavailable == false">
+        {{ priceFormated }}
+      </div>
+      <button
+        class="add"
+        v-if="unavailable == false"
+        @click="add">
+        Comprar
+      </button>
+      <div
+        class="unavailable"
+        v-if="unavailable">
+        Temporalmente no disponible
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Numeral from 'numeral';
+
+export default Vue.extend({
+  name: 'widget-store-add-bag',
+  props: [
+    'itemId',
+    'price',
+  ],
+  computed: {
+    priceFormated() {
+      switch (this.price) {
+        case 0: {
+          return 0;
+        }
+        case undefined: {
+          return 0;
+        }
+        default: {
+          return Numeral(this.price).format('$ 0.00');
+        }
+      }
+    },
+    unavailable() {
+      switch (this.price) {
+        case 0: {
+          return true;
+        }
+        case undefined: {
+          return true;
+        }
+        default: {
+          return false;
+        }
+      }
+    }
+  },
+  methods: {
+    add() {
+      alert(this.itemId);
+    }
+  }
+})
+</script>
