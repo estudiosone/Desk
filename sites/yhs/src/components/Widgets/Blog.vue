42c68a2b8873a80cdb7310f85e295e1348c9136c
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-08 16:26:02 -0300

    Create Blog.vue

---
+++ sites/yhs/src/components/Widgets/Blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="widget-blog">
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+
+export default Vue.extend({
+  name: 'widget-blog',
+})
+</script>
