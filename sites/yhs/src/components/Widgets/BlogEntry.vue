69377411774df1bd8f170ee3c5f94df5a4f1ac35
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-08 16:25:59 -0300

    Create BlogEntry.vue

---
+++ sites/yhs/src/components/Widgets/BlogEntry.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="widget-blog-entry">
+    <header class="entry-header">
+      <h1 class="title">
+        {{ entry.Title }}
+      </h1>
+      <p class="entry-meta">
+        <span class="entry-date">
+          {{ entry.Date }}
+        </span>
+        <span class="entry-category">
+          {{ entry.Category }}
+        </span>
+      </p>
+      <el-divider/>
+    </header>
+    <section class="entry-content" v-html="entry.Content"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { type } from 'os';
+
+type WidgetBlogEntry = {
+  Title: String;
+  Date: Date;
+  Category: String;
+  Content: String;
+}
+
+const props = {
+  entry: {
+    type: Object as () => WidgetBlogEntry,
+  }
+}
+
+export default Vue.extend({
+  name: 'widget-blog-entry',
+  props,
+})
+</script>
