c8d88b95ff5e8cf370f8aee8746e968589cdeb04
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-10 13:16:36 -0300

    Update SiteHeader.vue

--- sites/yhs/src/components/SiteHeader.vue
+++ sites/yhs/src/components/SiteHeader.vue
@@ -1,31 +1,53 @@
 <template>
   <div class="header">
     <div class="content">
-      <el-button size="mini" style="border: none;">
+      <el-dropdown
+        trigger="click"
+        class="hidden-sm-and-up">
+        <span class="el-dropdown-link">
+          <img src="https://img.icons8.com/ios/16/000000/menu.png">
+        </span>            
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item
+              v-for="NavMenu in this.$store.state.Site.NavMenu"
+              :key="NavMenu.To"
+              @click.native="$router.push(NavMenu.To)">
+              {{ NavMenu.Name }}
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+
+
+
+
+      <!-- <el-button size="mini" style="border: none;">
         <img src="https://img.icons8.com/ios/16/000000/menu.png">
-      </el-button>
+      </el-button> -->
       <div class="info">
         <div class="telefono">
           <img src="https://img.icons8.com/ios/16/000000/phone-filled.png">
           <span>+598 2601 7321</span>
         </div>
+        <div class="logo" >
+          <router-link to="/" tag="img" :src="this.$store.state.Site.LogoURL" />
+        </div>
       </div>
       <div class="w-buscar">
-        <el-button size="mini" style="border: none;">
+        <el-button type="text" size="mini" style="border: none;">
           <img src="https://img.icons8.com/ios/16/000000/search.png">
-          Buscar
+          <span>Buscar</span>
         </el-button>
       </div>
       <div class="w-mi-cuenta">
-        <el-button size="mini" style="border: none;">
+        <el-button type="text" size="mini" style="border: none;">
           <img src="https://img.icons8.com/ios/16/000000/gender-neutral-user-filled.png">
-          Mi Cuenta
+          <span>Mi Cuenta</span>
         </el-button>
       </div>
       <div class="w-bolsa-de-compra">
-        <el-button size="mini" style="border: none;">
+        <el-button type="text" size="mini" style="border: none;">
           <img src="https://img.icons8.com/ios/16/000000/shopping-bag-filled.png">
-          $ 0.00
+          <span>$ 0.00</span>
         </el-button>
       </div>
       <div class="principal">
@@ -54,8 +76,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+@import '../styles/bp';
+
 %boton {
   .el-button {
+    padding: 7px 15px;
+    color: black;
     font-family: 'Mukta Mahee', sans-serif;
     text-transform: uppercase;
   }
@@ -84,7 +110,7 @@ export default Vue.extend({
     justify-content: space-between;
     align-items: center;
     flex-flow: row wrap;
-    position: relative;
+    box-sizing: border-box;
   }
 
   .info {
@@ -92,36 +118,65 @@ export default Vue.extend({
     display: flex;
     order: 1;
     flex-grow: 100;
-  }
-  .telefono {
-    display: flex;
-    align-items: center;
-    font-family: 'Mukta Mahee', sans-serif;
-    font-size: 16px;
-    font-weight: 600;
-    img {
-      margin-right: 12px;
+    .telefono {
+      display: none;
+      align-items: center;
+      font-family: 'Mukta Mahee', sans-serif;
+      font-size: 16px;
+      font-weight: 600;
+
+      @include sm {
+        display: flex;
+      }
+
+      img {
+        margin-right: 12px;
+      }
+    }
+    .logo {
+      img {
+        height: 32px;
+        margin: 4px 0 4px 8px;
+        cursor: pointer;
+      }
+      @include sm {
+        display: none;
+      }
     }
   }
   .w-buscar {
-    @extend %boton-con-icono;
     order: 2;
   }
   .w-mi-cuenta {
-    @extend %boton-con-icono;
     order: 3;
   }
   .w-bolsa-de-compra {
-    @extend %boton-con-icono;
     order: 4;
+  }
+  .w-buscar,.w-mi-cuenta,.w-bolsa-de-compra {
+    @extend %boton-con-icono;
+    .el-button {
+      span {
+        span {
+          display: none;
+
+          @include sm {
+            display: flex;
+          }
+        }
+      }
+    }
   }
   .principal {
     height: 70px;
     order: 5;
+    display: none;
     flex-basis: 100%;
-    display: flex;
     align-items: center;
+    @include sm {
+          display: flex;
 
+    }
     .logo {
       flex-grow: 100;
       img {
@@ -134,8 +189,6 @@ export default Vue.extend({
       display: flex;
       justify-content: flex-end;
       .el-button {
-        padding: 7px 15px;
-        color: black;
         font-size: 18px;
       }
     }
