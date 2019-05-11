3969be6b5259a79434110781b143424361add2f5
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-11 13:30:47 -0300

    Update Home.vue

--- sites/yhs/src/views/Home.vue
+++ sites/yhs/src/views/Home.vue
@@ -38,37 +38,6 @@ export default Vue.extend({
   components: {
     WidgetBookingSalon,
   },
-  mounted() {
-    // const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // ui.start('#firebaseui-auth-container', {
-    //   autoUpgradeAnonymousUsers: true,
-    //   signInFlow: 'popup',
-    //   signInOptions: [
-    //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   ],
-    //   callbacks: {
-    //     // signInFailure callback must be provided to handle merge conflicts which
-    //     // occur when an existing credential is linked to an anonymous user.
-    //     signInFailure(error) {
-    //       // For merge conflicts, the error.code will be
-    //       // 'firebaseui/anonymous-upgrade-merge-conflict'.
-    //       if (error.code != 'firebaseui/anonymous-upgrade-merge-conflict') {
-    //         return Promise.resolve();
-    //       }
-    //       // The credential the user tried to sign in with.
-    //       const cred = error.credential;
-    //       // Copy data from anonymous user to permanent user and delete anonymous
-    //       // user.
-    //       // ...
-    //       // Finish sign-in after data is copied.
-    //       firebase.auth()
-    //        .currentUser.delete().then(() => firebase.auth().signInWithCredential(cred));
-    //     },
-    //   },
-    //   // Other config options...
-    // });
-  },
 });
 </script>
 
