ada5eb6df3025f122a2d955059d6150427fa13fe
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-12 10:35:17 -0300

    Update index.ts

--- plugins/desksuite/src/store/index.ts
+++ plugins/desksuite/src/store/index.ts
@@ -1,4 +1,25 @@
+enum BP {
+  xs = 'xs',
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg',
+  xl = 'xl',
+}
+
 export const Registrar = (store: any) => {
+  store.registerModule('Utilidades', {
+    namespaced: true,
+    state: {
+      Modales: {
+        Autenticacion: false
+      }
+    },
+    mutations: {
+      Modal_Autenticacion(state: any) {
+        state.Modales.Autenticacion = !state.Modales.Autenticacion;
+      }
+    }
+})
   store.registerModule('DatosEstaticos', {
     namespaced: true,
     state: {
@@ -26,4 +47,5 @@ export const Registrar = (store: any) => {
 
 export default {
   Registrar,
+  BP,
 }
