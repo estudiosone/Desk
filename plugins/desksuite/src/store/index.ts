6327fd9cdcbceeaa2cfe2f5c3edb049dc81c3d6f
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-12 19:26:08 -0300

    Update index.ts

--- plugins/desksuite/src/store/index.ts
+++ plugins/desksuite/src/store/index.ts
@@ -1,25 +1,98 @@
-enum BP {
-  xs = 'xs',
-  sm = 'sm',
-  md = 'md',
-  lg = 'lg',
-  xl = 'xl',
-}
-
 export const Registrar = (store: any) => {
   store.registerModule('Utilidades', {
     namespaced: true,
     state: {
+      UI: {
+        BP: {
+          xs: false,
+          xsUp: false,
+          smDown: false,
+          sm: false,
+          smUp: false,
+          mdDown: false,
+          md: false,
+          mdUp: false,
+          lgDown: false,
+          lg: false,
+          lgUp: false,
+          xlDown: false,
+          xl: false,
+        },
+      },
       Modales: {
-        Autenticacion: false
-      }
+        Autenticacion: false,
+      },
     },
     mutations: {
-      Modal_Autenticacion(state: any) {
-        state.Modales.Autenticacion = !state.Modales.Autenticacion;
-      }
-    }
-})
+      Modal_Autenticacion(state: any, payload: boolean = !state.Modales.Autenticacion) {
+        state.Modales.Autenticacion = payload;
+      },
+      UI_BP(state: any, payload: number) {
+        const sm = 768;
+        const md = 997;
+        const lg = 1200;
+        const xl = 1920;
+
+        const BP = {
+          xs: true,
+          xsUp: false,
+          smDown: false,
+          sm: false,
+          smUp: false,
+          mdDown: false,
+          md: false,
+          mdUp: false,
+          lgDown: false,
+          lg: false,
+          lgUp: false,
+          xlDown: false,
+          xl: false,
+        };
+
+        if (payload < sm) {
+          BP.xs = true;
+          BP.smDown = true;
+          BP.mdDown = true;
+          BP.lgDown = true;
+          BP.xlDown = true;
+          BP.xsUp = true;
+        } else if (payload >= sm && payload < md) {
+          BP.sm = true;
+          BP.smDown = true;
+          BP.smDown = true;
+          BP.mdDown = true;
+          BP.lgDown = true;
+          BP.xlDown = true;
+          BP.xsUp = true;
+          BP.smUp = true;
+        } else if (payload >= md && payload < lg) {
+          BP.md = true;
+          BP.mdDown = true;
+          BP.lgDown = true;
+          BP.xlDown = true;
+          BP.xsUp = true;
+          BP.smUp = true;
+          BP.mdUp = true;
+        } else if (payload >= lg && payload < xl) {
+          BP.lg = true;
+          BP.lgDown = true;
+          BP.xlDown = true;
+          BP.xsUp = true;
+          BP.smUp = true;
+          BP.mdUp = true;
+          BP.lgUp = true;
+        } else if (payload >= xl) {
+          BP.xl = true;
+          BP.xlDown = true;
+          BP.xsUp = true;
+          BP.smUp = true;
+          BP.mdUp = true;
+          BP.lgUp = true;
+        }
+        state.UI.BP = BP;
+      },
+    },
+  });
   store.registerModule('DatosEstaticos', {
     namespaced: true,
     state: {
@@ -33,19 +106,18 @@ export const Registrar = (store: any) => {
             Pais: 'Uruguay',
             Codigo: '+598',
           },
-        ]
+        ],
       },
       Identidad: {
         TipoDeDocumento: [
           'CI',
           'DNI',
-        ]
-      }
-    }
+        ],
+      },
+    },
   });
-}
+};
 
 export default {
   Registrar,
-  BP,
-}
+};
