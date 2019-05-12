2fdc95a42dc850f3d568849a9dcbbe7f02301996
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-12 19:29:02 -0300

    Update MiCompra.vue

---
+++ sites/yhs/src/views/MiCompra.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-mi-compra">
+    <div class="s-encabezado">
+      <div class="s-titulo">
+        mi compra
+      </div>
+    </div>
+    <div class="s-contenido">
+      <el-menu
+      class="s-menu">
+
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'p-mi-compra',
+
+});
+</script>
