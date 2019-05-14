27101fec378398ee704df501ff83439dad7b1144
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-14 17:43:04 -0300

    Create _direcciones-estados.ts

---
+++ plugins/desksuite/src/store/datos-estaticos/_direcciones-estados.ts
@@ -0,0 +1,59 @@
+export default [
+  {
+    "Nombre": "Canelones"
+  },
+  {
+    "Nombre": "Maldonado"
+  },
+  {
+    "Nombre": "Rocha"
+  },
+  {
+    "Nombre": "Treinta Y Tres"
+  },
+  {
+    "Nombre": "Cerro Largo"
+  },
+  {
+    "Nombre": "Rivera"
+  },
+  {
+    "Nombre": "Artigas"
+  },
+  {
+    "Nombre": "Salto"
+  },
+  {
+    "Nombre": "Paysandu"
+  },
+  {
+    "Nombre": "Rio Negro"
+  },
+  {
+    "Nombre": "Soriano"
+  },
+  {
+    "Nombre": "Colonia"
+  },
+  {
+    "Nombre": "San Jose"
+  },
+  {
+    "Nombre": "Flores"
+  },
+  {
+    "Nombre": "Florida"
+  },
+  {
+    "Nombre": "Lavalleja"
+  },
+  {
+    "Nombre": "Durazno"
+  },
+  {
+    "Nombre": "Tacuarembo"
+  },
+  {
+    "Nombre": "Montevideo"
+  }
+]
