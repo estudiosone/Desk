57837f97c758852655c8ff89f6c8b4e6b09eee0a
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-14 17:43:07 -0300

    Create index.ts

---
+++ plugins/desksuite/src/store/datos-estaticos/index.ts
@@ -0,0 +1,28 @@
+import DireccionesEstados from './_direcciones-estados';
+import DireccionesCiudades from './_direcciones-ciudades';
+
+export default {
+  namespaced: true,
+  state: {
+    DireccionesEstados,
+    DireccionesCiudades,
+    Telefono: {
+      Areas: [
+        {
+          Pais: 'Argentina',
+          Codigo: '+54',
+        },
+        {
+          Pais: 'Uruguay',
+          Codigo: '+598',
+        },
+      ],
+    },
+    Identidad: {
+      TipoDeDocumento: [
+        'CI',
+        'DNI',
+      ],
+    },
+  },
+};
