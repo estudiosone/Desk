4793b0bf8908dcfb29d54c953f3abef13e45eaa0
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-23 18:33:20 -0300

    Update index.ts

--- plugins/desksuite/src/store/index.ts
+++ plugins/desksuite/src/store/index.ts
@@ -1,12 +1,12 @@
-import utilities from './utilities/index';
-import datosEstaticos from './datos-estaticos';
+import utilities from "./utilities/index";
+import datosEstaticos from "./datos-estaticos";
 
 /* eslint-disable no-param-reassign */
 export const Registrar = (store: any) => {
-  store.registerModule('Utilidades', utilities);
-  store.registerModule('DatosEstaticos', datosEstaticos);
+  store.registerModule("Utilidades", utilities);
+  store.registerModule("DatosEstaticos", datosEstaticos);
 };
 
 export default {
-  Registrar,
+  Registrar
 };
