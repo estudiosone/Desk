2c05b4f74000ba054baa4944701f79991e524751
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-28 16:53:55 -0300

    Update Blog.ts

--- sites/yhs/src/views/Blog.ts
+++ sites/yhs/src/views/Blog.ts
@@ -2,22 +2,16 @@ import Vue from "vue";
 export default Vue.extend({
   name: "blog",
   filters: {
-    extractContent(s: any) {
-      return new DOMParser().parseFromString(s, "text/html").documentElement
-        .textContent;
-      // const span = document.createElement("span");
-      // span.innerHTML = s;
-      // const children = span.querySelectorAll("*");
-      // for (const item of children) {
-      //   if (item.textContent) {
-      //     item.textContent += " ";
-      //   } else {
-      //     item.innerText += " ";
-      //   }
-      // }
-      // return [span.textContent || span.innerText]
-      //   .toString()
-      //   .replace(/ +/g, " ");
+    extractContent(childrens: any) {
+      let text = '';
+
+      for (const children of childrens) {
+        if (children.type === 'p') {
+          text += `${children.data} `;
+        }
+      }
+
+      return text;
     }
   }
 });
