28f09e6264aefdbedf8ae9440f66ac7ebf592a4b
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-05-28 16:40:17 -0300

    Update BlogEntry.vue

--- sites/yhs/src/views/BlogEntry.vue
+++ sites/yhs/src/views/BlogEntry.vue
@@ -1,8 +1,13 @@
 <template lang="pug">
-  .p-blog
+  .p-blog-entry
     .s-encabezado
       .s-title {{ title }}
-    .s-contenido(v-html="content")
+    .s-contenido
+      .s-children(v-for="children in childrens",:key="children.index")
+        el-carousel(v-if="children.type === 'img'",:interval="4000",type="card",height="400px")
+          el-carousel-item(v-for="img in children.data",)
+            img(:src="img")
+        p(v-else-if="children.type === 'p'") {{children.data}}
 
 </template>
 
